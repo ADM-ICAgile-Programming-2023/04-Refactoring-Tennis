bdda8d91e574ebd6b28133413785e493a4c2d356
Fereidouni Yasser <Yasser.Fereidouni@admiral.at>
2023-12-04 10:52:54 +0100

    some more refactor was done.

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -29,11 +29,7 @@ namespace Tennis
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = ScoreAreAdvantage(m_score1, m_score2);
             }
             else
             {
@@ -58,6 +54,17 @@ namespace Tennis
                     }
                 }
             }
+            return score;
+        }
+
+        private string ScoreAreAdvantage(int m_score1, int m_score2)
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
             return score;
         }
 
