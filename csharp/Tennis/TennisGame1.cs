51f465111685b9fa4a123b0056dc39862792caf8
Andrii Kondratenko <Andrii.Kondratenko@admiral.at>
2023-12-04 11:22:49 +0100

    refactor: code style

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -24,21 +24,17 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                score = ScoreAreEqual(m_score1);
+                return ScoreAreEqual(m_score1);
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+
+            if (m_score1 >= 4 || m_score2 >= 4)
             {
-                score = ScoreAreAdvantage(m_score1, m_score2);
+                return ScoreAreAdvantage(m_score1, m_score2);
             }
-            else
-            {
-                score = translateScores(m_score1) + "-" + translateScores(m_score2);
-            }
-            return score;
+
+            return translateScores(m_score1) + "-" + translateScores(m_score2);
         }
 
         private string translateScores(int score)
@@ -60,13 +56,18 @@ namespace Tennis
 
         private string ScoreAreAdvantage(int m_score1, int m_score2)
         {
-            string score;
             var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-            return score;
+
+            if (minusResult == 1) return "Advantage player1";
+
+            if (minusResult == -1)
+            {
+                return "Advantage player2";
+            }
+
+            if (minusResult >= 2) return "Win for player1";
+
+            return "Win for player2";
         }
 
         private string ScoreAreEqual(int scoreInt)
