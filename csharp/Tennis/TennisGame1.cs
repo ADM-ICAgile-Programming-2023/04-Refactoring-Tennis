3797b4693e584ce1f439d1a6a9511fdfdb6363bd
Kevin Farr <kevin.farr@admiral.at>
2023-12-04 11:06:37 +0100

    refactor: extract default logic

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -35,24 +35,30 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    score = ScoreDefault(i, score, tempScore);
                 }
+            }
+            return score;
+        }
+
+        private string ScoreDefault(int i, string score, int tempScore) 
+        {
+            if (i == 1) tempScore = m_score1;
+            else { score += "-"; tempScore = m_score2; }
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
             return score;
         }
